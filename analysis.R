53dee81e6e2c073912afd6d43042550dcc47d659
njahn82 <najko.jahn@gmail.com>
2019-07-29 14:58:35 +0000

    update script

--- analysis.R
+++ analysis.R
@@ -8,3 +8,8 @@ goaj_clean <- goaj %>%
 # obtain url status code running on parallel cores
 jn_status_df <- plyr::llply(goaj_clean$url_tidy, purrr::safely(jn_status), 
                             .progress = "text")
+error_msg <- purrr::map(jn_status_df, "error")
+error_msg_character <- tibblemap_chr(error_msg, toString)
+data_frame(error = error_msg_character, tidy_url = goaj_clean$url_tidy) %>%
+  filter(grepl("error", error, ignore.case = TRUE)) %>%
+  write_csv("data/error_log.csv")
